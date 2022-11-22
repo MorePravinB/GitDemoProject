ef02c6f469f77537a500616acdfdbfe678fba9df
Admin <Admin@HP>
2022-11-22 10:37:43 +0530

    commiting factorial no branchx

---
+++ src/test1/factorialNO.java
@@ -0,0 +1,21 @@
+package test1;
+
+import java.util.Scanner;
+
+public class factorialNO {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any Number");
+		n = sc.nextInt();
+		int fact = 1;
+		for(int i =1; i<=n; i++)
+		{
+			fact =fact*i;
+		}
+		System.out.println("Factorial of given number is:"+fact);
+
+	}
+
+}
